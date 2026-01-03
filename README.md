9eb4288a18069b0a3a08ea20b43413c72c04914b
mav01-code <marreddyakshayavarshini@gmail.com>
2026-01-03 23:21:02 +0530

    Updated README

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # SOCIETY DESIGN
 
 This project aims to act as a medium of safety for the residents living in gated communities. It's still in the design process. The final design will be released soon.
+
+### Packages
+- database
+    - to connect database and backend using bean class (Base class in FastAPI)
+- entry
+- gatepass
+- residents
--- residents/service.py
+++ residents/service.py
@@ -1,18 +1,19 @@
 from sqlalchemy.orm import Session
 from .models import Resident
 
-def create_resident(db: Session, name, authorization, total_family_members, block, flat_number):
+# CRUD Operations for Resident
+def create_resident(db: Session, name, authorization, total_family_members, block, flat_number): # CREATE
     resident = Resident(name = name, authorization = authorization, total_family_members = total_family_members, block = block, flat_number = flat_number)
     db.add(resident)
     db.commit()
     db.refresh(resident)
     return resident
 
-def get_resident(db: Session, block, flat_number):
+def get_resident(db: Session, block, flat_number): # RETRIEVE
     return db.query(Resident).filter(Resident.block == block, Resident.flat_number == flat_number).first() 
     # first() returns first matched record or None instead of returning an sql object
 
-def update_resident(db: Session, name, authorization, total_family_members, block, flat_number):
+def update_resident(db: Session, name, authorization, total_family_members, block, flat_number): # UPDATE
     resident = get_resident(db, block, flat_number)
     if not resident:
         return None
@@ -23,7 +24,7 @@ def update_resident(db: Session, name, authorization, total_family_members, bloc
     db.refresh(resident)
     return resident
 
-def delete_resident(db: Session, block, flat_number):
+def delete_resident(db: Session, block, flat_number): # DELETE
     resident = get_resident(db, block, flat_number)
     if not resident:
         return None
