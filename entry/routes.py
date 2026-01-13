d91e345dff010f80a38631466c59265bf74725ae
mav01-code <marreddyakshayavarshini@gmail.com>
2026-01-13 23:41:31 +0530

    Initialized API for entry module

--- entry/routes.py
+++ entry/routes.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
-
+from sqlalchemy.orm import Session
+from database.db import get_db
+from entry.service import (
+    create_entry_request,
+    get_entry,
+    update_entry
+)
 router = APIRouter()
 
-@router.get("/ping")
-def ping():
-    return {"status": "Gatepass service alive"}
--- gatepass/routes.py
+++ gatepass/routes.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from database.db import get_db
 
 router = APIRouter()
 
