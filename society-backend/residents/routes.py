092709cb672f7d2f5b478e3acf8629ce05fa639e
mav01-code <marreddyakshayavarshini@gmail.com>
2026-01-19 12:16:16 +0530

    Completed Resident info API by username

--- society-backend/residents/routes.py
+++ society-backend/residents/routes.py
@@ -19,6 +19,14 @@ def add_resident(resident: ResidentCreate, db: Session = Depends(get_db)):
 def fetch_resident(block: str, flat_number: str, db: Session = Depends(get_db)):
     return get_resident(db, block, flat_number)
 
+@router.get("{block}-{flat_number}")
+def fetch_resident_username(username: str, db: Session = Depends(get_db)):
+    username = username.strip()
+    l = username.split("-")
+    block = l[0]
+    flat_number = l[1]
+    return get_resident(db, block, flat_number)
+
 @router.put("/{block}/{flat_number}")
 def update_res(block: str, flat_number: str, data: ResidentUpdate, db: Session = Depends(get_db)):
     return update_resident(db, data.name, data.authorization, data.total_family_members, block, flat_number)
